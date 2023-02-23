745a20cd341ce59029ba07fe199540b09104ed38
diut <1029296449@qq.com>
2023-02-23 12:53:04 +0800

    1

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -5,14 +5,39 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        colorList: [],
+        isloading: false
+    },
+    getColors(){
+        this.setData({
+            isloading: true
+        })
+        //需要展示loading效果
+        wx.showLoading({
+          title: '数据加载中...',
+        })
+        wx.request({
+          url: 'https://www.escook.cn/api/color',
+          method: 'GET',
+          success: ({data: res})=>{
+            this.setData({
+                colorList: [...this.data.colorList, ...res.data]
+            })
+          },
+          complete: ()=>{
+              wx.hideLoading(),
+              this.setData({
+                isloading: false
+            })
+          }
+        })
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-
+        this.getColors()
     },
 
     /**
@@ -40,7 +65,7 @@ Page({
      * 生命周期函数--监听页面卸载
      */
     onUnload() {
-
+        
     },
 
     /**
@@ -54,7 +79,9 @@ Page({
      * 页面上拉触底事件的处理函数
      */
     onReachBottom() {
-
+        if (this.data.isloading)  return 
+        
+        this.getColors()
     },
 
     /**
